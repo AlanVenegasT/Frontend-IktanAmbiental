4c6bea2e0ea216eeb1d798bbb8dd875e96800e45
AFE <afe95xd@gmail.com>
2023-02-02 15:49:08 -0600

    agregando footer y modificacion contacto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Avion from '../components/Avion'
-import Contacto from '../components/Contacto'
-import Cumplimiento from '../components/Cumplimiento'
-import Hero from '../components/Hero'
-import Imagen2en1 from '../components/Imagen2en1'
-import Somos from '../components/Somos'
-import Tecnologia from '../components/Tecnologia'
+import React from "react";
+import Avion from "../components/Avion";
+import Contacto from "../components/Contacto";
+import Cumplimiento from "../components/Cumplimiento";
+import Hero from "../components/Hero";
+import Imagen2en1 from "../components/Imagen2en1";
+import Somos from "../components/Somos";
+import Tecnologia from "../components/Tecnologia";
 
 const Home = () => {
   return (
     <>
-      <Hero/>
-      <Somos/>
-      
-      <Cumplimiento/>  
-      <Tecnologia/> 
-       
-      <Avion/>  
-      
-       
-      <Contacto/>  
-    </>
-  )
-}
+      <Hero />
+      <Somos />
 
-export default Home
+      <Cumplimiento />
+      <Tecnologia />
+
+      <Avion />
+
+      <Contacto />
+    </>
+  );
+};
+
+export default Home;
